f845a48fd08e6cc99067658915ec449d65abc6c3
Claudson Martins <claudson@outlook.com>
2016-04-14 22:00:34 -0300

    Create palindromo.hs

---
+++ haskell/exercicio_2/palindromo.hs
@@ -0,0 +1,110 @@
+import Data.Char (isSpace)
+--main = putStrLn "hello world"
+
+{-diga_oi :: IO ()
+
+diga_oi = do
+
+  putStrLn "Qual eh o seu nomezinho?"
+  name <- getLine
+  putStrLn $ "Oi, " ++ name
+
+main = diga_oi
+-}
+
+zeroToN :: Int -> [Int]
+zeroToN x = [0..x]
+
+add1ToFst :: [Int] -> [Int]
+add1ToFst [] = []
+add1ToFst (x:xs) = (x+1:xs)
+
+add1ToLst :: [Int] -> [Int]
+add1ToLst [] = []
+add1ToLst (x:[]) = (x+1:[])
+add1ToLst (x:xs) = (x:add1ToLst xs)
+
+len :: [a] -> Int
+len [] = 0
+len (x:xs) = 1 + len xs
+
+maior :: [Int] -> Int
+maior [] = 0
+maior (x:[]) = x
+maior (x:xs) | x > (maior xs) = x
+             | otherwise = (maior xs)
+             
+
+removeItem :: Int -> [Int] -> [Int]
+removeItem _ []                 = []
+removeItem x (y:ys) | x == y    = removeItem x ys
+                    | otherwise = y : removeItem x ys
+
+-- Ler lista
+
+lerlista :: IO [Int]
+lerlista = do 
+              linha <- getLine
+              return (read linha :: [Int])
+           
+lerstring :: IO String
+lerstring = do
+              linha1 <- getLine
+              if linha1 == []
+                 then return []
+                 else do
+                        linha2 <- lerstring
+                        if linha2 == []
+                           then return linha1
+                           else return (linha1 ++ "," ++ linha2)
+
+lerlistaint :: IO [Int]
+lerlistaint = do
+                linha <- lerstring
+                return (read ("[" ++ linha ++ "]") :: [Int])
+
+
+
+
+
+
+
+
+palindromo :: IO Bool
+palindromo = do
+                entrada <- getLine
+                return ((rev (removeSpaces entrada)) == (removeSpaces entrada))
+
+rev xs =    if null xs
+            then xs
+            else rev (tail xs) ++ [head xs]
+
+-- Remove espaços
+removeSpaces :: String -> String
+removeSpaces "" = ""
+removeSpaces x = if (head x) /= ' ' then (head x) : removeSpaces (tail x) else removeSpaces (tail x) -- se encontrar espaço, não insere na nova lista
+
+
+trim xs = dropSpaceTail "" $ dropWhile isSpace xs
+
+dropSpaceTail maybeStuff "" = ""
+dropSpaceTail maybeStuff (x:xs)
+        | isSpace x = dropSpaceTail (x:maybeStuff) xs
+        | null maybeStuff = x : dropSpaceTail "" xs
+        | otherwise       = reverse maybeStuff ++ x : dropSpaceTail "" xs
+
+--main = print $ zeroToN 10
+--main = print $ add1ToFst [8,5,4,9,7]
+--main = print $ len ['A','R','I','E','L', 'A']
+--main = print $ maior [4,2,9,8,10]
+--main = print $ exercicioRemoveElemento
+main :: IO ()
+main = do
+        entrada <- palindromo
+        --valor <- rev entrada
+        print $ entrada
+
+
+--reverse = foldl (flip (:)) []
+
+--main = print $ removeSpaces "amor e                            roma"
