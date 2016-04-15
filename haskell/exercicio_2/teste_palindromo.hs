7b5c6382baf35c5d76612d72096c682731c419c3
Claudson Martins <claudson@outlook.com>
2016-04-15 11:19:15 -0300

    MDC

--- haskell/exercicio_2/teste_palindromo.hs
+++ haskell/exercicio_2/teste_palindromo.hs
@@ -1,34 +1,6 @@
 import Data.Char
 import Data.List
 
-removeItem :: Int -> [Int] -> [Int]
-removeItem _ []                 = []
-removeItem x (y:ys) | x == y    = removeItem x ys
-                    | otherwise = y : removeItem x ys
-
--- Ler lista
-lerlista :: IO [Int]
-lerlista = do 
-              linha <- getLine
-              return (read linha :: [Int])
-           
-lerstring :: IO String
-lerstring = do
-              linha1 <- getLine
-              if linha1 == []
-                 then return []
-                 else do
-                        linha2 <- lerstring
-                        if linha2 == []
-                           then return linha1
-                           else return (linha1 ++ "," ++ linha2)
-
-lerlistaint :: IO [Int]
-lerlistaint = do
-                linha <- lerstring
-                return (read ("[" ++ linha ++ "]") :: [Int])
-
-
 rev :: [a] -> [a]
 rev []      = [] 
 rev (x:xs)  = reverse xs ++ [x]
