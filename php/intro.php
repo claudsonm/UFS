f83e27f316c0d3c31eac272771986c5241a70c11
Claudson Martins <claudson@outlook.com>
2016-05-12 17:26:35 -0300

    Exemplos PHP e algoritmos prolog

---
+++ php/intro.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Exemplo</title>
+    </head>
+    <body>
+        <?php
+            echo "Olá, eu sou um script PHP!";
+        ?>
+    </body>
+</html>
