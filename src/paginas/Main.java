0b3bfee236af5490405a80869c8a740be2410a65
Claudson Martins <claudson@outlook.com>
2016-09-24 17:23:16 -0600

    Algoritmos de substituição de páginas finalizados

--- src/paginas/Main.java
+++ src/paginas/Main.java
@@ -8,31 +8,23 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner entrada = new Scanner(new File("entrada_p.txt"));
-        int quadros = entrada.nextInt();
-        
         ArrayList<Integer> lista = new ArrayList<>();
+        int quadros = entrada.nextInt();
         
         while( entrada.hasNext() ) {
             lista.add(entrada.nextInt());
         }
         entrada.close();
 
-        //processaFIFO(lista, quadros);
+        processaFIFO(lista, quadros);
         processaOTM(lista, quadros);
-    }
-    
-    public static void imprimaMemoria(List<Integer> m) {
-        System.out.print("MEMORIA [");
-        for (Integer i : m) {
-            System.out.print(i + " ");
-        }
-        System.out.println("]");
+        processaLRU(lista, quadros);
     }
     
     public static void processaFIFO(List<Integer> lista, int q) {
         List<Integer> memoria = Arrays.asList(new Integer[q]);
-        int faltas = 0;
-        int j = 0;
+        int faltas = 0, j = 0;
+        
         for (Integer i : lista) {
             if (! memoria.contains(i)) {
                 faltas++;
@@ -47,46 +39,91 @@ public class Main {
     
     public static void processaOTM(List<Integer> lista, int q) {
         List<Integer> memoria = Arrays.asList(new Integer[q]);
-        int faltas = 0;
-        int j = 0;
+        int faltas = 0, j = 0;
+        
         for (Integer i : lista) {
             if (! memoria.contains(i)) {
                 faltas++;
-                calculaProxUso(lista, memoria, j);
-                memoria.set(j++ % q, i);
+                memoria.set(melhorTroca(lista, memoria, j), i);
             }
-            else {
-                memoria.set(j % q, i);
-            }
-            //imprimaMemoria(memoria);
+            j++;
         }
-        System.out.println("FIFO " + faltas);
+        System.out.println("OTM " + faltas);
     }
     
-    public static int calculaProxUso(List<Integer> lista, List<Integer> memoria, int j) {
+    public static void processaLRU(List<Integer> lista, int q) {
+        List<Integer> memoria = Arrays.asList(new Integer[q]);
+        int faltas = 0, j = 0;
+        
+        for (Integer i : lista) {
+            if (! memoria.contains(i)) {
+                faltas++;
+                memoria.set(trocaRecente(lista, memoria, j), i);
+            }
+            j++;
+        }
+        System.out.println("LRU " + faltas);
+    }
+    
+    public static int melhorTroca(List<Integer> lista, List<Integer> memoria, int j) {
         int cont = 0;
         int[] diferencas = new int[memoria.size()];
-        int inicio = j;
         
         // Para cada elemento da memoria
         for (int i = 0, t = memoria.size(); i < t; i++) {
+            if (memoria.get(i) == null) return i;
             // Percorre a lista partindo de J
-            for (int k = j; k < lista.size(); k++) {
-                if (lista.get(j++) != memoria.get(i)) {
-                    cont++;
+            for (int k = j, t2 = lista.size(); k < t2; k++) {
+                cont++;
+                if (lista.get(k) == memoria.get(i)) {
+                    break;
                 }
-                else break;
             }
             diferencas[i] = cont;
             cont = 0;
-            j = inicio;
         }
         
-        for (int i = 0; i < diferencas.length; i++) {
-            System.out.print(diferencas[i] + " ");
+        return posicaoMaiorElemento(diferencas);
+    }
+    
+    public static int trocaRecente(List<Integer> lista, List<Integer> memoria, int j) {
+        int cont = 0;
+        int[] diferencas = new int[memoria.size()];
+        
+        // Para cada elemento da memoria
+        for (int i = 0, t = memoria.size(); i < t; i++) {
+            if (memoria.get(i) == null) return i;
+            // Percorre a lista partindo de J
+            for (int k = j; k > 0; k--) {
+                cont++;
+                if (lista.get(k) == memoria.get(i)) {
+                    break;
+                }
+            }
+            diferencas[i] = cont;
+            cont = 0;
         }
-        System.out.println("\n**************");
-        return 0;
+        
+        return posicaoMaiorElemento(diferencas);
+    }
+    
+    public static int posicaoMaiorElemento(int[] v){
+        int index = -1, maior = -1;
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] > maior) {
+                maior = v[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    
+    public static void imprimaMemoria(List<Integer> m) {
+        System.out.print("MEMORIA [");
+        for (Integer i : m) {
+            System.out.print(i + " ");
+        }
+        System.out.println("]");
     }
 
 }
