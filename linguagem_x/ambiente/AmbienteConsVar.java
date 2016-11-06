29eb59ce8e583c34ca73c9153fd215d1c9758bb1
Claudson Martins <claudson@outlook.com>
2016-11-05 23:40:51 -0300

    Reformatação manual seguindo padrões do Google

--- linguagem_x/ambiente/AmbienteConsVar.java
+++ linguagem_x/ambiente/AmbienteConsVar.java
@@ -1,61 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Claudson Martins
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package ambiente;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
-
 import checagem.*;
 
 /**
- * Gerencia a tabela de símbolos de variáveis e constantes.
- * Atua como uma fábrica dos Flyweights para gerar objetos da classe concreta
- * VinculavelConsVar.
+ * Gerencia a tabela de símbolos de variáveis e constantes do Checker. Atua como uma fábrica dos
+ * flyweights para gerar objetos da classe concreta VinculavelConsVar.
+ *
+ * @author Claudson Martins
+ * @author Edgar Lima
+ * @author Guilherme Boroni
  */
 public class AmbienteConsVar {
+
     /**
-     * Hash Identificador -> VinculavelConsVar, que contém os dados sobre tipo. 
+     * Tabela hash que mapeia o nome de um identificador para uma lista de VinculavelConsVar, que
+     * contém as informações dos tipos dos identificadores de mesmo nome.
      */
     public static final HashMap<String, ArrayList<VinculavelConsVar>> tabela =
             new HashMap<String, ArrayList<VinculavelConsVar>>();
     
-    /**
-     * Nível atual de escopo da tabela de símbolos
-     */
+    /** Nível atual de escopo da tabela de símbolos. */
     public static int nivel = 0;
     
-    /**
-     * Pilha que contém a lista com todos os identificadores de cada escopo.
-     */
-    public static Stack<ArrayList<String>> simbolosEscopo =
-            new Stack<ArrayList<String>>();
+    /** Pilha que contém a lista com o nome de todos os identificadores de cada escopo. */
+    public static Stack<ArrayList<String>> simbolosEscopo = new Stack<ArrayList<String>>();
     
     /**
      * Adiciona um identificador na lista do hash da tabela de símbolos.
      * 
-     * @param id
-     * @param x
-     * @return
+     * @param id nome do identificador
+     * @param x informações sobre o identificador
      */
     private void adicionaTabela(String id, VinculavelConsVar x) {
         ArrayList<VinculavelConsVar> listaItens = tabela.get(id);
-        
         // Se a lista da tabela ainda não existe, cria
         if (listaItens == null) {
             listaItens = new ArrayList<VinculavelConsVar>();
             listaItens.add(x);
             tabela.put(id, listaItens);
+        } else {
+            listaItens.add(x);
         }
-        else listaItens.add(x);
-        
-        System.out.println("Criando ConsVar no ambiente no [Escopo " + nivel
-                + "]: " + (x.isVar ? "var" : "cons") + " " + x.tipo + " " + id);
+        System.out.println("Criando ConsVar no ambiente no [Escopo " + nivel + "]: " +
+                (x.isVar ? "var" : "cons") + " " + x.tipo + " " + id);
     }
     
     /**
      * Remove um identificador da tabela de símbolos.
      * 
-     * @param id
-     * @return
+     * @param id nome do identificador
+     * @return informações sobre o identificador removido
      */
     private VinculavelConsVar deletaTabela(String id) {
         ArrayList<VinculavelConsVar> l = tabela.get(id);
@@ -63,17 +84,16 @@ public class AmbienteConsVar {
     }
     
     /**
-     * Adiciona um identificador na pilha e na tabela de símbolos.
+     * Adiciona o nome do identificador na pilha e as informações sobre ele na tabela de símbolos.
      * 
-     * @param id
-     * @param b
-     * @param t
-     * @return
+     * @param id nome do identificador
+     * @param b true representa uma variável, false representa uma constante
+     * @param t informações sobre o tipo
+     * @return true se adicionou com sucesso, false caso tenha sido possível
      */
     public boolean add(String id, boolean b, TipoSemantico t) {
         boolean r = false;
         ArrayList<String> listaPilha;
-        
         if (simbolosEscopo.isEmpty()) {
             listaPilha = new ArrayList<String>();
             listaPilha.add(id);
@@ -81,25 +101,24 @@ public class AmbienteConsVar {
             VinculavelConsVar x = new VinculavelConsVar(b, t, nivel);
             this.adicionaTabela(id, x);
             r = true;
-        }
-        else {
+        } else {
             listaPilha = simbolosEscopo.peek();
-            if (listaPilha.contains(id)) System.out.println("Ja tem [" + b + " " + t + " " + id + "] !");
-            else {
+            if (listaPilha.contains(id)) {
+                System.out.println("Ja tem [" + b + " " + t + " " + id + "] !");
+            } else {
                 listaPilha.add(id);
                 VinculavelConsVar x = new VinculavelConsVar(b, t, nivel);
                 this.adicionaTabela(id, x);
                 r = true;
             }
         }
-        
         return r;
     }
     
     /**
-     * Remove um identificador da pilha e da tabela de símbolos.
+     * Remove o nome do identificador da pilha e as informações de tipo da tabela de símbolos.
      * 
-     * @param id
+     * @param id nome do identificador
      */
     public void deleta(String id) {
         ArrayList<String> listaPilha = simbolosEscopo.peek();
@@ -108,10 +127,10 @@ public class AmbienteConsVar {
     }
     
     /**
-     * Retorna a instância mais recente de um elemento na tabela de símbolos.
+     * Retorna a instância mais recente de um identificador na lista da tabela de símbolos.
      * 
-     * @param id
-     * @return
+     * @param id nome do identificador
+     * @return informações sobre o identificador
      */
     public VinculavelConsVar get(String id) {
         ArrayList<VinculavelConsVar> l = tabela.get(id);
@@ -119,28 +138,24 @@ public class AmbienteConsVar {
     }
     
     /**
-     * Retorna true se o identificador existe no escopo atual.
+     * Verifica se o identificador existe no escopo atual.
      * 
-     * @param id
-     * @return
+     * @param id nome do identificador
+     * @return true caso já exista, false caso não exista
      */
     public boolean contem(String id) {
         ArrayList<String> listaPilha = simbolosEscopo.peek();
         return listaPilha.contains(id);
     }
     
-    /**
-     * Incrementa um nível de escopo
-     */
+    /** Incrementa um nível de escopo */
     public void comecaEscopo() {
         ArrayList<String> listaPilha = new ArrayList<String>();
         nivel++;
         simbolosEscopo.push(listaPilha);
     }
     
-    /**
-     * Decrementa um nível de escopo
-     */
+    /** Decrementa um nível de escopo */
     public void terminaEscopo() {
         ArrayList<String> listaPilha = simbolosEscopo.pop();
         for (String st : listaPilha)
--- linguagem_x/ambiente/AmbienteFuncProc.java
+++ linguagem_x/ambiente/AmbienteFuncProc.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Claudson Martins
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package ambiente;
 
 import java.util.HashMap;
@@ -5,65 +29,72 @@ import java.util.List;
 import checagem.*;
 
 /**
- * Atua como uma fábrica dos Flyweights para gerar objetos da classe concreta
- * VinculavelFuncProc
+ * Gerencia a tabela de símbolos de funções e procedimentos do Checker. Atua como uma fábrica dos
+ * flyweights para gerar objetos da classe concreta VinculavelFuncProc.
+ *
+ * @author Claudson Martins
+ * @author Edgar Lima
+ * @author Guilherme Boroni
  */
 public class AmbienteFuncProc {
+
+    /**
+     * Tabela hash que mapeia o nome de um identificador para uma Vinculavel, que contém as
+     * informações sobre os tipos das funções ou procedimentos.
+     */
     public static final HashMap<String, Vinculavel> tabela = new HashMap<>();
     
     /**
-     * Procura ou cria na tabela de símbolos uma função
+     * Recupera ou cria na tabela de símbolos as informações sobre uma <b>função</b>.
      * 
-     * @param id
-     * @param p
-     * @param r
-     * @return
+     * @param id nome do identificador
+     * @param p lista de tipos dos parâmetros formais
+     * @param r informações sobre o tipo de retorno
+     * @return informações sobre a função
      */
-    public VinculavelFuncProc lookupFuncProc(String id,
-            List<PassagemTipoSemantico> p, TipoSemantico r) {
+    public VinculavelFuncProc lookupFuncProc(String id, List<PassagemTipoSemantico> p,
+            TipoSemantico r) {
         VinculavelFuncProc x = (VinculavelFuncProc) tabela.get(id);
-        if(x == null) {
+        if (x == null) {
             x = new VinculavelFuncProc(p, r);
             tabela.put(id, x);
-            System.out.println("Criando FuncProc no ambiente : func " + id
-                    + "(" + x.paramFunc + ")");
+            System.out.println("Criando FuncProc no ambiente: func " + id + "(" + x.paramFunc + ")");
         }
         return x;
     }
     
     /**
-     * Procura ou cria na tabela de símbolos um procedimento
+     * Recupera ou cria na tabela de símbolos as informações sobre um <b>procedimento</b>.
      * 
-     * @param id
-     * @param p
-     * @return
+     * @param id nome do identificador
+     * @param p lista de tipos dos parâmetros formais
+     * @return informações sobre a procedimento
      */
     public VinculavelFuncProc lookupFuncProc(String id, List<PassagemTipoSemantico> p) {
         VinculavelFuncProc x = (VinculavelFuncProc) tabela.get(id);
-        if(x == null) {
+        if (x == null) {
             x = new VinculavelFuncProc(p);
             tabela.put(id, x);
-            System.out.println("Criando FuncProc no ambiente : proc "
-                    + id + "(" + x.paramProc + ")");
+            System.out.println("Criando FuncProc no ambiente: proc " + id + "(" + x.paramProc + ")");
         }
         return x;
     }
     
     /**
-     * Procura a função ou procedimento dado o nome do identificador
+     * Recupera da tabela de símbolos a função ou procedimento.
      * 
-     * @param id Nome da função ou procedimento
-     * @return
+     * @param id nome da função ou procedimento
+     * @return informações sobre a função ou procedimento
      */
     public VinculavelFuncProc get(String id) {
         return (VinculavelFuncProc) tabela.get(id);
     }
     
     /**
-     * Retorna true se o identificador já existe na tabela de símbolos.
+     * Verifica se o identificador já existe na tabela de símbolos.
      * 
-     * @param id Nome da função ou procedimento a ser verificado
-     * @return
+     * @param id nome do identificador
+     * @return true caso já exista, false caso não exista
      */
     public boolean contem(String id) {
         return tabela.containsKey(id);
